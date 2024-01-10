72e05c7a042630efadbe769a1ac22cb7868dd76d
Aliz <alicja_bobko@o2.pl>
2024-01-10 11:34:15 +0100

    metareducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -14,4 +14,14 @@ export const reducers: ActionReducerMap<AppState> = {
   router: routerReducer,
 };
 
-export const metaReducers: MetaReducer<AppState>[] = isDevMode() ? [] : [];
+export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
+  return (state, action) => {
+    console.log("state before: ", state);
+    console.log("action", action);
+    return reducer(state, action);
+  };
+}
+
+export const metaReducers: MetaReducer<AppState>[] = isDevMode()
+  ? [logger]
+  : [];
